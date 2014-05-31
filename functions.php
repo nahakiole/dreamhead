afff160095344e6c5b596081ba42047c2d3c8896
Manuel Meister <news.manuelsworld@gmail.com>
2014-05-31 16:56:12 +0200

    Added SVG Support for Media Upload

--- functions.php
+++ functions.php
@@ -145,6 +145,10 @@ function get_navigation() {
 
 }
 
-
+function cc_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
 
 add_theme_support('post-formats', array('video', 'gallery', 'audio'));
