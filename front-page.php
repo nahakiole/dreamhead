3ce8b145237aa129a2c502212af14cd56d41fa33
Nahakiole <robin.glauser@gmail.com>
2014-06-21 23:30:40 +0200

    New Frontpage design.

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
         $paged = 1;
     }
 
-    query_posts('posts_per_page=10&paged=' . $paged);
+    query_posts('posts_per_page=11&paged=' . $paged);
 
     $count = 0;
     while (have_posts()) {
@@ -23,15 +23,15 @@
             $color = empty($color) ? 'transparent' : $color;
             $subtitle = get_post_meta(get_the_ID(), 'DREAMHEAD_subtitle', true);
             $image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-            $svgClass = substr($image,-3,3) == 'svg' ? 'svg' : '';
+            $svgClass = substr($image, -3, 3) == 'svg' ? 'svg' : '';
             ?>
 
             <!-- Main jumbotron for a primary marketing message or call to action -->
             <a href="<?php the_permalink() ?>">
-            <div class="jumbotron main-article <?php echo $svgClass ?>"
-                 style="background-image: url('<?php
-                 echo $image;
-                 ?>'); background-color: <?php echo $color?>">
+                <div class="jumbotron main-article <?php echo $svgClass ?>"
+                     style="background-image: url('<?php
+                     echo $image;
+                     ?>'); background-color: <?php echo $color ?>">
 
                     <div class="article-pretext">
                         <div class="container">
@@ -67,27 +67,27 @@
         }
         ?>
         <div class="col-md-12 top60 article-preview">
-            <div class="row">
-                <div class="col-lg-3 col-lg-offset-1 col-md-5">
 
-                    <?php
-                    echo get_the_post_thumbnail(null, 'large', "class=img-responsive")
-                    ?>
+            <div class="col-lg-6 col-md-12">
 
-                </div>
-                <div class="caption col-lg-7 col-md-7">
-                    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                    <span class="autor-name"> Am <?php the_date('d.m.Y'); ?> von  <?php echo get_the_author(); ?></span>
+                <?php
+                echo get_the_post_thumbnail(null, 'large', "class=img-responsive")
+                ?>
 
-                    <p> <?php
-                        echo substr(strip_tags(get_the_excerpt()), 0, 240) . "..." ?></p>
-                </div>
+            </div>
+            <div class="caption col-lg-6 col-md-12">
+                <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                <span class="autor-name"> Am <?php the_date('d.m.Y'); ?> von  <?php echo get_the_author(); ?></span>
+
+                <p> <?php
+                    echo substr(strip_tags(get_the_excerpt()), 0, 240) . "..." ?></p>
             </div>
             <a class="read-more"
-                href="<?php the_permalink() ?>">Weiterlesen
+               href="<?php the_permalink() ?>">Weiterlesen
             </a>
-
         </div>
+
+
 
     <?php
     }
